25a2179bbff8cdf3c92b65374d8b9991d938f677
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-18 14:31:06 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.querySelector('#root');
+
+const element = (
+  <div className="greeting">
+    <div className="greeting__title">Hello, world!</div>
+    <div className="greeting__text">I'm learning React</div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, rootElement);
